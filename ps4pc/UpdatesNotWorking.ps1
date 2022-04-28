4b4976fdb8ebade0bb1c6b676552869355be8691
FrankieFee <ffeola@gmail.com>
2022-04-28 10:11:25 -0400

    added requirement for curl downloads.

--- ps4pc/UpdatesNotWorking.ps1
+++ ps4pc/UpdatesNotWorking.ps1
@@ -1,3 +1,5 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls, [Net.SecurityProtocolType]::Tls11, [Net.SecurityProtocolType]::Tls12, [Net.SecurityProtocolType]::Ssl3
+[Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
 Set-Location HKLM:\
 Get-ChildItem 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing\Packages\' | Where-Object { $_.Name -like '*Package_*_for_KB3186568~31bf3856ad364e35~amd64~~10.0.1.1044*' } | foreach { Set-ItemProperty $($_.Name+"*") -Name CurrentState -Value 0 }
 Get-ChildItem 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing\Packages\' | Where-Object { $_.Name -like '*Package_*_for_KB4033393~31bf3856ad364e35~amd64~~10.0.1.1549*' } | foreach { Set-ItemProperty $($_.Name+"*") -Name CurrentState -Value 0 } Get-ChildItem 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing\Packages\' | Where-Object { $_.Name -like '*Package_*_for_KB4054855~31bf3856ad364e35~amd64~~10.0.1.1554*' } | foreach { Set-ItemProperty $($_.Name+"*") -Name CurrentState -Value 0 }
